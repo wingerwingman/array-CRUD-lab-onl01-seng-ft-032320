dd0b1df3952f0c5c5c79d3ceef6e630509f0975a
Justin Winger <justinwinger4@gmail.com>
2020-03-24 16:35:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  an_array = [1, 2]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.pop[element]
 end
 
 def add_element_to_start_of_array(array, element)
